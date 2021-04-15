fc45db0fce2aad9681cecac41a10b15994904044
mouzedrift <mouzedrift@gmail.com>
2021-04-15 06:49:57 +0200

    fix wrong cache find path

--- Source/resizeablerectitem.cpp
+++ Source/resizeablerectitem.cpp
@@ -193,9 +193,9 @@ void ResizeableRectItem::Init()
     // Allow select and move.
     setFlags( ItemSendsScenePositionChanges |  ItemSendsGeometryChanges | ItemIsMovable | ItemIsSelectable );
 
-    if ( !QPixmapCache::find( mMapObject->mObjectStructureType.c_str(), m_Pixmap ) )
+    QString images_path = ":/object_images/rsc/object_images/";
+    if ( !QPixmapCache::find(images_path + mMapObject->mObjectStructureType.c_str() + ".bmp", m_Pixmap ) )
     {
-        QString images_path = ":/object_images/rsc/object_images/";
         m_Pixmap = QPixmap(images_path + mMapObject->mObjectStructureType.c_str() + ".bmp");
         QPixmapCache::insert(images_path + mMapObject->mObjectStructureType.c_str() + ".bmp", m_Pixmap );
     }
