6bc2ef023e02ea6fe74bbf945cf250ff556381fc
Paul <github@paulsapps.com>
2021-05-09 20:58:53 +0100

    impl undo/redo for BasicTypeProperty

--- Source/BasicTypeProperty.hpp
+++ Source/BasicTypeProperty.hpp
@@ -1,12 +1,32 @@
 #pragma once
 
 #include "PropertyTreeItemBase.hpp"
+#include <QUndoCommand>
 
 struct BasicType;
 class QUndoStack;
+class BigSpinBox;
 
-class BasicTypeProperty final : public PropertyTreeItemBase
+class ChangeBasicTypePropertyCommand : public QUndoCommand
 {
+public:
+    ChangeBasicTypePropertyCommand(PropertyTreeWidget* pTreeWidget, ObjectProperty* pProperty, BasicType* pBasicType, int oldValue, int newValue);
+
+    void undo() override;
+
+    void redo() override;
+
+private:
+    PropertyTreeWidget* mTreeWidget = nullptr;
+    ObjectProperty* mProperty = nullptr;
+    BasicType* mBasicType = nullptr;
+    int mOldValue = 0;
+    int mNewValue = 0;
+};
+
+class BasicTypeProperty final : public QObject, public PropertyTreeItemBase
+{
+    Q_OBJECT
 public:
     BasicTypeProperty(QUndoStack& undoStack, QTreeWidgetItem* pParent, QString propertyName, ObjectProperty* pProperty, BasicType* pBasicType);
 
@@ -17,8 +37,12 @@ public:
         return mProperty;
     }
 
+    void Refresh() override;
+
 private:
     QUndoStack& mUndoStack;
     ObjectProperty* mProperty = nullptr;
     BasicType* mBasicType = nullptr;
+    int mOldValue = 0;
+    BigSpinBox* mSpinBox = nullptr;
 };
