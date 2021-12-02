07dd03721091e993f494e906ce8200d078f04930
mouzedrift <mouzedrift@gmail.com>
2021-12-02 23:40:50 +0100

    WIP dark stylesheet/design and add the old editor icon

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,10 +1,19 @@
 #include "editormainwindow.hpp"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     EditorMainWindow w;
+
+    QFile File(":/stylesheets/rsc/stylesheets/dark-stylesheet.qss");
+    File.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(File.readAll());
+
+    w.setStyleSheet(styleSheet);
+    w.setWindowIcon(QIcon(":/icons/rsc/icons/icon.png"));
+
     w.show();
     return a.exec();
 }
