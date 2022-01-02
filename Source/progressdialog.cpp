9f8b7fe1674f5926fdb218758623c3e20ed5a1d9
Paul <github@paulsapps.com>
2022-01-02 15:47:03 +0000

    Async save

---
+++ Source/progressdialog.cpp
@@ -0,0 +1,14 @@
+#include "progressdialog.hpp"
+#include "ui_progressdialog.h"
+
+ProgressDialog::ProgressDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ProgressDialog)
+{
+    ui->setupUi(this);
+}
+
+ProgressDialog::~ProgressDialog()
+{
+    delete ui;
+}
