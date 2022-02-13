0f0a543bdac1f35284bf4f795671f80d8253b583
Sita <kamilsitarski98@gmail.com>
2022-02-13 23:48:34 +0100

    add the option to export json to lvl via the cmd args

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,8 +3,14 @@
 #include <QFile>
 #include <QTranslator>
 #include <QDebug>
+#include "exporter.hpp"
+#include <functional>
+#include <QtCore/qcommandlineparser.h>
+#include "main.h"
 
 void DoMapSizeTests();
+
+int exportJsonToLvl(const QStringList& args);
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +26,23 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.installTranslator(&translator);
 
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addPositionalArgument("source", QCoreApplication::translate("main", "Source file."));
+    parser.addPositionalArgument("dest", QCoreApplication::translate("main", "Destination file."));
+
+    QCommandLineOption exportJsonToLvlOption("export", QCoreApplication::translate("main", "Export the .json file to the .lvl file. Usage: --export source dest"));
+    parser.addOption(exportJsonToLvlOption);
+
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+
+    if (parser.isSet(exportJsonToLvlOption))
+    {
+        return exportJsonToLvl(args);
+    }
+
     EditorMainWindow w;
 
     QFile File(":/stylesheets/rsc/stylesheets/dark-stylesheet.qss");
@@ -32,4 +55,22 @@ int main(int argc, char *argv[])
 
     w.show();
     return app.exec();
+}
+
+int exportJsonToLvl(const QStringList& args)
+{
+    if (args.size() != 2)
+    {
+        std::cerr << "Incorrect usage of the --export option, should be --export source dest" << std::endl;
+        return 1;
+    }
+    const QString source = args.at(0);
+    const QString destination = args.at(1);
+    int runResult = 0;
+    exportJsonToLvl(source, destination, "relive_export", [&runResult](const QString& text) mutable
+        {
+            std::cerr << "Exporting failed. " << text << std::endl;
+            runResult = 1;
+        });
+    return runResult;
 }
